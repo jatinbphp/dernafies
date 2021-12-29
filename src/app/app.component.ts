9e01133fc97d44aa727b45de283eac5c6722ad7a
Jatin Bhatt <jatin.b.php@gmail.com>
2021-12-29 13:10:27 +0530

    profile screen related changes made

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { ClientService } from './providers/client.service';
+import { ModalController } from '@ionic/angular';
+import { ProfilePage } from './profile/profile.page';
 
 @Component({
   selector: 'app-root',
@@ -14,13 +16,13 @@ export class AppComponent
   public appPages = [
     //{ title: 'Sign In', url: '/sign-in', icon: 'person'},//[0]
     //{ title: 'Sign Up', url: '/sign-up', icon: 'person'},//[0]
-    { title: 'Profile', url: '/tabs/profile', icon: 'person', is_function:0},//[0]
+    { title: 'Profile', url: '/tabs/profile', icon: 'person', is_function:1},//[0]
     { title: 'Current Requests', url: '/tabs/current-requests', icon: 'reorder-four', is_function:0},//[1]    
     { title: 'Past Requests', url: '/tabs/past-requests', icon: 'time', is_function:0},//[2]    
     { title: 'Settings', url: '/tabs/settings', icon: 'settings', is_function:0},//[3]    
     { title: 'Logout', url: '/tabs/home', icon: 'power', is_function:1},//[4]    
   ];
-  constructor(public client: ClientService) 
+  constructor(public client: ClientService, public modalCtrl: ModalController) 
   {
     this.client.getObservableOnLanguageChange().subscribe((data) => {
       this.language_selected = data.language_selected;
@@ -48,6 +50,23 @@ export class AppComponent
     this.appPages[2].title = this.default_language_data['translation'][0]['menu'][0][this.language_selected][0]['past_requests'];
     this.appPages[3].title = this.default_language_data['translation'][0]['menu'][0][this.language_selected][0]['settings'];
     this.appPages[4].title = this.default_language_data['translation'][0]['menu'][0][this.language_selected][0]['logout'];
+  }
+
+  async showMyProfile()
+  {
+    let id = (localStorage.getItem('id')) ? localStorage.getItem('id') : undefined;
+    if(id!='' && id!='null' && id!=null && id!=undefined && id!='undefined')
+    {
+      const modal = await this.modalCtrl.create({
+        component: ProfilePage,
+      });
+
+      return await modal.present();
+    }
+    else 
+    {
+      this.client.router.navigate(['sign-in']);  
+    }
   }
 
   Logout()
