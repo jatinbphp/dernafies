9d8b75cc237b8782d1eea54e11859b204b1473f2
Trushna Nxsol <trushna.k.designer@gmail.com>
2021-12-29 12:15:29 +0530

    pg design

--- src/app/search/search.page.ts
+++ src/app/search/search.page.ts
@@ -13,37 +13,6 @@ export class SearchPage {
 
   constructor(public fb: FormBuilder, public client: ClientService, public menu: MenuController, public loadingCtrl: LoadingController, public modalCtrl: ModalController) 
   {}
-  
-  categorieSlide = {
-    // slidesPerView: 1.3,
-    initialSlide: 1,
-    slidesPerView: this.categoriecheckScreen(),
-    speed: 600,
-  };
-  
-  categoriecheckScreen() {
-    let innerWidth = window.innerWidth;
-    switch (true) {
-      case 320 <= innerWidth && innerWidth <= 374:
-        return 1.1;
-      case 375 <= innerWidth && innerWidth <= 475:
-        return 2.6;
-      case 476 <= innerWidth && innerWidth <= 575:
-        return 2.6;
-      case 576 <= innerWidth && innerWidth <= 700:
-        return 1.8;
-      case 701 <= innerWidth && innerWidth <= 900:
-        return 2.3;
-      case 901 <= innerWidth && innerWidth <= 991:
-        return 3.3;
-      case 992 <= innerWidth && innerWidth <= 1025:
-        return 3.3;
-      case 1026 <= innerWidth && innerWidth <= 1199:
-        return 3.3;
-      case 1200 <= innerWidth:
-        return 4.3;
-    }
-  }
 
   async showMyProfile()
   {
