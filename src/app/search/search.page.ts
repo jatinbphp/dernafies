06afc09a279c1bf1fc30c14ff864cbf17d4f72ce
Jatin Bhatt <jatin.b.php@gmail.com>
2022-02-22 19:44:10 +0530

    search related changes are made

--- src/app/search/search.page.ts
+++ src/app/search/search.page.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
-import { MenuController, LoadingController, ModalController } from '@ionic/angular';
+import { Platform, MenuController, LoadingController, ModalController } from '@ionic/angular';
 import { ClientService } from '../providers/client.service';
 import { FormBuilder, Validators } from '@angular/forms';
 import { ProfilePage } from '../profile/profile.page';
 import { NavigationExtras } from "@angular/router";
+import { Geolocation } from '@awesome-cordova-plugins/geolocation/ngx';
+import { NativeGeocoder, NativeGeocoderResult, NativeGeocoderOptions } from '@awesome-cordova-plugins/native-geocoder/ngx';
+import { LocationAccuracy } from '@awesome-cordova-plugins/location-accuracy/ngx';
+import { AndroidPermissions } from '@awesome-cordova-plugins/android-permissions/ngx';
 
 @Component({
   selector: 'app-search',
@@ -48,8 +52,13 @@ export class SearchPage
     //subHeader: 'Select your hair color',
     //message: 'Only select your dominant hair color'
   };
+  
+  public locationCordinates: any;
+  public timestamp: any;
+  public current_latitude:any = '';
+  public current_longitude:any = '';
 
-  constructor(public fb: FormBuilder, public client: ClientService, public menu: MenuController, public loadingCtrl: LoadingController, public modalCtrl: ModalController) 
+  constructor(public fb: FormBuilder, public client: ClientService, public menu: MenuController, public loadingCtrl: LoadingController, public modalCtrl: ModalController, private geolocation: Geolocation, private platform: Platform, private nativeGeocoder: NativeGeocoder, private androidPermissions: AndroidPermissions, private locationAccuracy: LocationAccuracy) 
   {
     this.default_language_data = this.client.default_language_data;
 		this.language_selected = this.client.language_selected;
@@ -94,18 +103,19 @@ export class SearchPage
     this.tempH=0;//RESET DATA
 
     this.search_for_handyman=JSON.parse(localStorage.getItem('search_for_handyman'));
-    //REQUIRED TO CONTINUE TOWARDS JOB BOOKING PROCESS
-    let obj_way_to_select_handyman = 
-    {
-        handyman_category_id:0,
-        latitude:(this.search_for_handyman.latitude) ? this.search_for_handyman.latitude : "",
-        longitude:(this.search_for_handyman.longitude) ? this.search_for_handyman.longitude : "",
-        to_be_show_featured_handyman:"no"
-    };
-    localStorage.setItem("way_to_select_handyman",JSON.stringify(obj_way_to_select_handyman));
-    //REQUIRED TO CONTINUE TOWARDS JOB BOOKING PROCESS
     if(this.search_for_handyman!=null && this.search_for_handyman!=undefined && this.search_for_handyman!='null' && this.search_for_handyman!='undefined')
     {
+      //REQUIRED TO CONTINUE TOWARDS JOB BOOKING PROCESS
+      let obj_way_to_select_handyman = 
+      {
+          handyman_category_id:0,
+          latitude:(this.search_for_handyman.latitude) ? this.search_for_handyman.latitude : "",
+          longitude:(this.search_for_handyman.longitude) ? this.search_for_handyman.longitude : "",
+          to_be_show_featured_handyman:"no"
+      };
+      localStorage.setItem("way_to_select_handyman",JSON.stringify(obj_way_to_select_handyman));
+      //REQUIRED TO CONTINUE TOWARDS JOB BOOKING PROCESS
+
       this.search_text = (this.search_for_handyman.keyword) ? this.search_for_handyman.keyword : "";
       //LOADER
       const loading = await this.loadingCtrl.create({
@@ -153,6 +163,9 @@ export class SearchPage
               userTypeID:this.resultDataSearch[h].userTypeID,
               no_of_experience:this.resultDataSearch[h].no_of_experience,
               price:this.resultDataSearch[h].price,
+              total_ratings:this.resultDataSearch[h].total_ratings,
+              pricingTypeName:(this.resultDataSearch[h].pricingTypes.pricingTypeName) ? this.resultDataSearch[h].pricingTypes.pricingTypeName : "",
+              unitPricingType:(this.resultDataSearch[h].pricingTypes.unitPricingType) ? this.resultDataSearch[h].pricingTypes.unitPricingType : "",
             }
             this.resultDataSearchPerSlideTemp.push(objHandyMan);
             if(this.tempH % this.handyManToBeShowOnSingleSlide == 0)
@@ -201,6 +214,28 @@ export class SearchPage
       //console.log("MAX",this.handyMaxPrice);
       //LOOKING FOR MIN AND MAX PRICE VALUE
     }
+    else
+    {
+      this.locationCordinates = 
+      {
+        latitude: "",
+        longitude: "",
+        accuracy: "",
+        timestamp: ""
+      }
+      this.timestamp = Date.now();
+      await this.platform.ready().then(async () => 
+      {
+        if(this.platform.is("android") == true)
+        {
+          this.checkPermission();
+        }
+        else 
+        {
+          await this.currentLocPosition();
+        }
+      });
+    }
     console.log(this.search_for_handyman);
   }
 
@@ -241,19 +276,99 @@ export class SearchPage
 
   searchForHandyMan(form)
   {
-    let searched_text = form.controls.search_text.value;
-    this.search_text = searched_text;
+    let searched_text = (form.controls.search_text.value) ? form.controls.search_text.value : "";
     let objSearch=
     {
       keyword:(searched_text) ? searched_text : "",
-      latitude:(this.search_for_handyman.latitude) ? this.search_for_handyman.latitude : "",
-      longitude:(this.search_for_handyman.longitude) ? this.search_for_handyman.longitude : "",
-      category_id:(this.search_for_handyman.category_id) ? this.search_for_handyman.category_id : 0,
-      experience:(this.search_for_handyman.experience) ? this.search_for_handyman.experience : 0,
-      price_range:(this.search_for_handyman.price_range) ? this.search_for_handyman.price_range : 0,
-      reviews:(this.search_for_handyman.reviews) ? this.search_for_handyman.reviews : 0,
+      latitude:(this.current_latitude) ? this.current_latitude : "",
+      longitude:(this.current_longitude) ? this.current_longitude : "",
+      category_id:0,
+      experience:0,
+      price_range:0,
+      reviews:0,
     }
     localStorage.setItem('search_for_handyman',JSON.stringify(objSearch));
     this.ionViewWillEnter();
+  }
+
+  checkPermission() 
+  {
+    this.androidPermissions.checkPermission(this.androidPermissions.PERMISSION.ACCESS_COARSE_LOCATION).then(
+      result => {
+        if (result.hasPermission) 
+        {
+          this.enableGPS();
+        } 
+        else 
+        {
+          this.locationAccPermission();
+        }
+      },
+      error => {
+        alert("1"+error);
+      }
+    );
+  }
+
+  locationAccPermission() 
+  {
+    this.locationAccuracy.canRequest().then((canRequest: boolean) => 
+    {
+      if (canRequest) {} 
+      else 
+      {
+        this.androidPermissions.requestPermission(this.androidPermissions.PERMISSION.ACCESS_COARSE_LOCATION)
+          .then(() => 
+            {
+              this.enableGPS();
+            },
+            error => 
+            {
+              alert("2"+error)
+            }
+          );
+      }
+    });
+  }
+
+  enableGPS() 
+  {
+    this.locationAccuracy.request(this.locationAccuracy.REQUEST_PRIORITY_HIGH_ACCURACY).then(
+      async () => {
+        await this.currentLocPosition();
+      },
+      error => {
+        alert("3"+JSON.stringify(error));
+      }
+    );
+  }
+
+  async currentLocPosition() 
+  {
+     //LOADER
+     const loadingLocation = await this.loadingCtrl.create({
+      spinner: null,
+      //duration: 5000,
+      message: 'Please wait...',
+      translucent: true,
+      cssClass: 'custom-class custom-loading'
+    });
+    await loadingLocation.present();
+    //LOADER
+    await this.geolocation.getCurrentPosition().then((response) => 
+    {
+      loadingLocation.dismiss();//DISMISS LOADER
+      this.locationCordinates.latitude = response.coords.latitude;
+      this.locationCordinates.longitude = response.coords.longitude;
+      this.locationCordinates.accuracy = response.coords.accuracy;
+      this.locationCordinates.timestamp = response.timestamp;
+    }).catch((error) => 
+    {
+      loadingLocation.dismiss();//DISMISS LOADER
+      alert('4-Error: ' + error);
+    });
+    
+    this.current_latitude=Number(this.locationCordinates.latitude);
+    this.current_longitude=Number(this.locationCordinates.longitude);
   }
 }
